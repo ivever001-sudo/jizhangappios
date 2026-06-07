20e779ec8ddc017322ff2dcd50d25abfce185265
ivever001-sudo <ivever001@gmail.com>
2026-06-07 21:40:36 +0800

    Update KSP version in build.gradle.kts

--- jizhangApp-ios/build.gradle.kts
+++ jizhangApp-ios/build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.10" apply false
     id("org.jetbrains.compose") version "1.8.2" apply false
     id("androidx.room") version "2.7.0-alpha13" apply false 
-    id("com.google.devtools.ksp") version "2.1.10-1.0.29" apply false
+    id("com.google.devtools.ksp") version "2.1.10-1.0.30" apply false
 }
